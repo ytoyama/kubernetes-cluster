a203478ee8f3a696c769882e1924e1326fed2ced
Yota Toyama <raviqqe@gmail.com>
2016-12-13 17:32:22 +0900

    Install kube tools

---
+++ Shakefile.hs
@@ -0,0 +1,14 @@
+import Development.Shake
+import Development.Shake.Command
+import Development.Shake.FilePath
+import Development.Shake.Util
+
+
+hostsFile = "hosts"
+
+
+main :: IO ()
+main = shakeArgs shakeOptions $ do
+  "ping" ~> cmd "ansible all -i" hostsFile "-m ping"
+  "setup" ~> do
+    cmd "ansible-playbook --ask-become-pass -i" hostsFile "setup.yml"
